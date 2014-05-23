321912cd9464bb4e93198898a5572ea3f0e23f6f
Darrell Simmons <drell424@gmail.com>
2014-05-23 11:24:25 -0400

    added photos model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523151506) do
+ActiveRecord::Schema.define(version: 20140523152220) do
 
   create_table "listings", force: true do |t|
     t.string   "address"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20140523151506) do
     t.text     "description"
     t.text     "details"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
+    t.integer  "listing_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
