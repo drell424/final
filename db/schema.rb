73a35054f90eb0545258b586e520834dc5ad953a
Darrell Simmons <drell424@gmail.com>
2014-05-23 11:21:49 -0400

    added root view via high voltage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523150650) do
+ActiveRecord::Schema.define(version: 20140523151506) do
+
+  create_table "listings", force: true do |t|
+    t.string   "address"
+    t.integer  "price"
+    t.text     "description"
+    t.text     "details"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
